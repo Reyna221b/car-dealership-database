aaae526f13981f0e5c64d98b10e3ee6d84c8e0c4
Reyna <Reyna221b@outlook.com>
2024-06-10 16:06:28 -0700

    completed

---
+++ tests.sql
@@ -0,0 +1,24 @@
+SELECT * 
+FROM dealerships;
+
+
+SELECT * FROM vehicles 
+WHERE dealership_id = (SELECT dealership_id FROM dealerships WHERE name = 'Dealership 1');
+
+
+SELECT * FROM vehicles WHERE VIN = '1FMCU0GD3CKA12345';
+
+
+SELECT * FROM dealerships 
+WHERE dealership_id = (SELECT dealership_id FROM inventory WHERE VIN = '1FMCU0GD3CKA12345');
+
+SELECT * FROM dealerships 
+WHERE dealership_id IN (SELECT dealership_id FROM vehicles WHERE make = 'Ford');
+
+
+
+SELECT * FROM sales_contracts 
+WHERE dealership_id = (SELECT dealership_id FROM dealerships WHERE name = 'Dealership 1') 
+AND sale_date BETWEEN '2022-01-01' AND '2022-12-31';
+
+
